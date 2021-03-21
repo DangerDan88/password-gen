db89b4a1976d73deb74dd4f38e59daf5fc0b9e75
Danger <51889207+DangerDan88@users.noreply.github.com>
2021-03-21 17:56:10 -0600

    need to find out how to concat all the randoms into a single line

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+//TODO figure out how to concat all the random fucntions into one password string
 document.addEventListener("DOMContentLoaded", function (event) {
   // random number function
   function getRandomInt() {
-    return Math.floor(Math.random() * 10);
+    return Math.floor(Math.random() * 10).toString();
   }
   getRandomInt();
 
@@ -9,8 +10,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
   function getRandomLowerCase() {
     const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    const randomCharacter =
-      alphabet[Math.floor(Math.random() * alphabet.length)];
+    const randomCharacter = alphabet[
+      Math.floor(Math.random() * alphabet.length)
+    ].toString();
   }
   getRandomLowerCase();
   // random uppercase function
@@ -20,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     let randomCharacter = alphabet[
       Math.floor(Math.random() * alphabet.length)
     ].toUpperCase();
+    console.log(randomCharacter);
   }
   getRandomUpperCase();
 
@@ -27,7 +30,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
     let symbols = "!@#$%^&*()";
 
     let randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
-    console.log(randomSymbol);
+    // console.log(randomSymbol);
   }
   getRandomSymbol();
+
+  document
+    .getElementById("generate")
+    .addEventListener("click", generatePassWord);
+
+  function generatePassWord() {
+    let newpassUpper = getRandomUpperCase();
+    let newPassLower = getRandomLowerCase();
+
+    document.getElementById("goesHere").innerHTML = newpassUpper;
+  }
 });
