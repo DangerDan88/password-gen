d31d124737eab387681b52c21091a3083533a5db
Danger <51889207+DangerDan88@users.noreply.github.com>
2021-03-31 12:30:52 -0600

    remaking password generator needed key codes to work

--- index.js
+++ index.js
@@ -1,47 +1,20 @@
-//TODO figure out how to concat all the random fucntions into one password string
+//TODO figure out how to concat all the random fucntions into one password string need to use character codes
 document.addEventListener("DOMContentLoaded", function (event) {
-  // random number function
-  function getRandomInt() {
-    return Math.floor(Math.random() * 10).toString();
-  }
-  getRandomInt();
-
-  // random lowercase function
-  function getRandomLowerCase() {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-    const randomCharacter = alphabet[
-      Math.floor(Math.random() * alphabet.length)
-    ].toString();
-  }
-  getRandomLowerCase();
-  // random uppercase function
-
-  function getRandomUpperCase() {
-    let alphabet = "abcdefghijklmnopqrstuvwxyz";
-    let randomCharacter = alphabet[
-      Math.floor(Math.random() * alphabet.length)
-    ].toUpperCase();
-    console.log(randomCharacter);
-  }
-  getRandomUpperCase();
-
-  function getRandomSymbol() {
-    let symbols = "!@#$%^&*()";
-
-    let randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
-    // console.log(randomSymbol);
-  }
-  getRandomSymbol();
-
-  document
-    .getElementById("generate")
-    .addEventListener("click", generatePassWord);
-
-  function generatePassWord() {
-    let newpassUpper = getRandomUpperCase();
-    let newPassLower = getRandomLowerCase();
-
-    document.getElementById("goesHere").innerHTML = newpassUpper;
-  }
+  // Character Code Generating Function
+  let arrayFromLowToHigh = (low, high) => {
+    const array = [];
+    for (let i = low; i <= high; i++) {
+      array.push(i);
+    }
+    return array;
+  };
+  // Generating Character Codes
+  const UPPERCASE_CODES = arrayFromLowToHigh(65, 90);
+  const LOWERCASE_CODES = arrayFromLowToHigh(97, 122);
+  const NUMBER_CODES = arrayFromLowToHigh(48, 57);
+  const SYMBOL_CODES = arrayFromLowToHigh(33, 47)
+    .concat(arrayFromLowToHigh(58, 64))
+    .concat(arrayFromLowToHigh(91, 96))
+    .concat(arrayFromLowToHigh(123, 126));
+  console.log(UPPERCASE_CODES);
 });
